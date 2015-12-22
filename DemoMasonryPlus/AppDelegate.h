d34fe01c7fff5e6eaab3fd9eba42c325888afcaf
bigcd <liushiq@liushiqdemacbook-pro.local>
2015-12-22 10:30:07 +0800

    Initial Commit

---
+++ DemoMasonryPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoMasonryPlus
+//
+//  Created by liushiq on 15/12/22.
+//  Copyright © 2015年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoMasonryPlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoMasonryPlus
+//
+//  Created by liushiq on 15/12/22.
+//  Copyright © 2015年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
